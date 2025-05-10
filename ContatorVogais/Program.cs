6aa19471147fa0e37f9fddc3a54ba92ce7a6c5f0
Zenx007 <zenxpokas@gmail.com>
2025-05-09 23:58:15 -0300

    init

---
+++ ContatorVogais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Vogais;
+
+class Contador
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma palavra");
+        string palavra = Console.ReadLine();
+
+        int totalVogais = ContarVogais(palavra);
+        Console.WriteLine($"A frase contém {totalVogais} vogais.");
+
+        static int ContarVogais(string texto)
+        {
+            int contador = 0;
+            string vogais = "aeiouAEIOU";
+
+            foreach (char letra in texto)
+            {
+                if (vogais.Contains(letra))
+                {
+                    contador++;
+                }
+            }
+
+            return contador;
+        }
+    }
+}
+
+
